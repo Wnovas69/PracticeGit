25957352cc7bf0991f3649aef400c7fcdfdfeb31
Wagner Jafet Matos Pe�a <20220137@itla.edu.do>
2023-10-02 14:31:32 -0400

    Suplidor funcionando con todos sus metodos.

--- MergePractice/Data/Entities/Suplidor.cs
+++ MergePractice/Data/Entities/Suplidor.cs
@@ -2,5 +2,10 @@
 {
     public class Suplidor
     {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public int? RNC { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
     }
 }
